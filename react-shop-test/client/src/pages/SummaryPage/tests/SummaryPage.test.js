2a1f0cf38a05e70a26ff2e9ba262fb8d0d86727e
Dongwook Kim <dongwook227@gmail.com>
2022-04-23 20:48:26 -0400

    add test util for wrapping context

--- react-shop-test/client/src/pages/SummaryPage/tests/SummaryPage.test.js
+++ react-shop-test/client/src/pages/SummaryPage/tests/SummaryPage.test.js
@@ -1,5 +1,5 @@
 import SummaryPage from "../SummaryPage";
-import { render, screen } from "@testing-library/react";
+import { render, screen } from "../../../test-utils";
 
 test("checkbox box and button", () => {
   render(<SummaryPage />);
