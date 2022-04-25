c28d752399b794b7f6b620778f776553f942f4dd
Dongwook Kim <dongwook227@gmail.com>
2022-04-24 20:51:07 -0400

    add test for options

--- react-shop-test/client/src/pages/OrderPage/tests/calculate.test.js
+++ react-shop-test/client/src/pages/OrderPage/tests/calculate.test.js
@@ -4,7 +4,7 @@ import { OrderContextProvider } from "../../../contexts/OrderContext";
 import Type from "../Type";
 
 test("update product's total when products change", async () => {
-  render(<Type orderType="products" />, { wrapper: OrderContextProvider });
+  render(<Type orderType="products" />);
   const totalElement = screen.getByText("총 가격", {
     exact: false,
   });
@@ -18,4 +18,29 @@ test("update product's total when products change", async () => {
   userEvent.type(americaInputElement, "1");
 
   expect(totalElement).toHaveTextContent("1000");
+});
+
+test("update options' total when options change", async () => {
+  // 옵션1을 클릭한다
+  // 가격을 확인한다
+  render(<Type orderType="options" />);
+  const optionsTotal = screen.getByText("총 가격", { exact: false });
+  expect(optionsTotal).toHaveTextContent("0");
+
+  // 옵션2를 클릭한다
+  // 가격을 확인한다
+  const option1 = await screen.findByRole("checkbox", {
+    name: "Insurance",
+  });
+  userEvent.click(option1);
+  expect(optionsTotal).toHaveTextContent("500");
+
+  // 옵션2를 다시 클릭한다
+  const option2 = await screen.findByRole("checkbox", { name: "Dinner" });
+  userEvent.click(option2);
+  expect(optionsTotal).toHaveTextContent("1000");
+
+  // 가격을 확인한다
+  userEvent.click(option2);
+  expect(optionsTotal).toHaveTextContent("500");
 });
