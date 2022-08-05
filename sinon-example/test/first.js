c802e5ad3d316d97ee297356069a64cc30dc2d95
Dongwook Kim <dongwook227@gmail.com>
2022-08-05 07:52:50 -0400

    update test code

--- sinon-example/test/first.js
+++ sinon-example/test/first.js
@@ -1,14 +1,14 @@
 const assert = require("assert");
 const greeter = require("../greeter.js");
+const sinon = require("sinon");
 
 describe("testing the greeter", function () {
-  if (
-    ("checks the greet function",
-    function () {
-      assert.equal(
-        greeter.greet("Alice"),
-        "Hello, Alice! Today is Friday, January 15, 2021."
-      );
-    })
-  );
+  it("checks the greet function", function () {
+    var clock = sinon.useFakeTimers(new Date(2021, 0, 15));
+    assert.equal(
+      greeter.greet("Alice"),
+      "Hello, Alice! Today is Friday, January 15, 2021"
+    );
+    clock.restore();
+  });
 });
