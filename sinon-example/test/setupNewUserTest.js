e8888b9e1e4ca5b9de1d929f9fa23fce5e9e1d7a
Dongwook Kim <dongwook227@gmail.com>
2022-08-06 17:13:50 -0400

    add example for sinon stub

--- sinon-example/test/setupNewUserTest.js
+++ sinon-example/test/setupNewUserTest.js
@@ -22,4 +22,40 @@ describe("testing setupNewUser", function () {
     save.restore();
     sinon.assert.calledWith(save, expectedUser);
   });
+
+  it("should pass object with correct values to save", function () {
+    var save = sinon.stub(Database, "save");
+    var info = { name: "John Doe" };
+    var expectedUser = {
+      name: info.name,
+      nameLowerCase: info.name.toLowerCase(),
+    };
+
+    setupNewUser(info, function () {});
+
+    save.restore();
+    sinon.assert.calledWith(save, expectedUser);
+  });
+
+  it("should pass the error into the callback if save fails", function () {
+    var expectedError = new Error("opps");
+    var save = sinon.stub(Database, "save");
+    save.throws(expectedError);
+    var callback = sinon.spy();
+
+    setupNewUser({ name: "foo" }, callback);
+    save.restore();
+    sinon.assert.calledWith(callback, expectedError);
+  });
+
+  it("should pass the database result into the callback", function () {
+    var expectedResult = { success: true };
+    var save = sinon.stub(Database, "save");
+    save.yields(null, expectedResult);
+    var callback = sinon.spy();
+    setupNewUser({ name: "foo" }, callback);
+
+    save.restore();
+    sinon.assert.calledWith(callback, null, expectedResult);
+  });
 });
